61840761e2ef36dda490c864d7578cac32a556e8
pawan10 <pawandev1099@gmail.com>
2021-06-14 18:28:46 +0530

    Summer of bitcoin task

---
+++ pawan.py
@@ -0,0 +1,22 @@
+import pandas as pd
+import os
+ 
+csv_file = (r"C:\Users\HP\Projects\SOB\mempool.csv")
+txt_file = (r"C:\Users\HP\Projects\SOB\Block.txt")
+
+text_list = []
+
+with open(csv_file, "r") as my_input_file:
+    for line in my_input_file:
+        line = line.split(",")
+        text_list.append(",".join(line))
+
+with open(txt_file, "w") as my_output_file:
+    #my_output_file.write("#1\n")
+   # my_output_file.write("double({},{})\n".format(len(text_list),2))
+  
+    for line in text_list:
+        my_output_file.write("  " + line)
+    print('File Successfully written.') 
+
+
